b8122a9f01462c4674b38d1ca3ded34b2ab8d177
Fred Buchanan <fred@pineapplecomputerart.com>
2014-11-26 21:23:26 -0500

    Did all kinds of stuff to make viewing messages work. Started work on creating chat.

--- messanger/database/__init__.py
+++ messanger/database/__init__.py
@@ -11,49 +11,61 @@ import sqlite3
 from flask import g
 
 #Global Deleratons--------------------------------------------------------------
+#annoying flask stuff
 db='database.db'
+from messanger import app
+#def connect_db():
+#    return sqlite3.connect('database.db')
+
+
+
+
 
 #Classes------------------------------------------------------------------------
 
 
 def make_conversation(user1,user2):
-    conn = sqlite3.connect(db)
-    c = conn.cursor()
-    c.execute("INSERT INTO conversations ('user1','user2','log') VALUES (?, ?, 'Conversations Started... \n')", (user1,user2))
-    conn.commit()
-    conn.close()
+    with app.app_context():
+        conn = sqlite3.connect(db)
+        c = conn.cursor()
+        c.execute("INSERT INTO conversations ('user1','user2','log') VALUES (?, ?, 'Conversations Started... \n')", (user1,user2))
+        conn.commit()
+        conn.close()
 
 def add(user1,user2,toadd):
-    conn = sqlite3.connect(db)
-    c =  conn.cursor()
-    c.execute("SELECT log FROM conversations WHERE user1=? AND user2=?",(user1,user2))
-    now = c.fetchone()
-    c.execute("UPDATE conversations SET log=? WHERE user1=? AND user2=?",(now[0]+toadd,user1,user2))
-    conn.commit()
-    conn.close()
+    with app.app_context():
+        conn = sqlite3.connect(db)
+        c =  conn.cursor()
+        c.execute("SELECT log FROM conversations WHERE user1=? AND user2=?",(user1,user2))
+        now = c.fetchone()
+        c.execute("UPDATE conversations SET log=? WHERE user1=? AND user2=?",(now[0]+toadd,user1,user2))
+        conn.commit()
+        conn.close()
 
 def get(user1,user2):
-    conn = sqlite3.connect(db)
-    c =  conn.cursor()
-    c.execute("SELECT log FROM conversations WHERE user1=? AND user2=?",(user1,user2))
-    out = c.fetchone()
-    conn.close()
-    return out[0]
+    with app.app_context():
+        conn = sqlite3.connect(db)
+        c =  conn.cursor()
+        c.execute("SELECT log FROM conversations WHERE user1=? AND user2=?",(user1,user2))
+        out = c.fetchone()
+        conn.close()
+        return out[0]
 
 def getMyConversations(user):
-    conn=sqlite3.connect(db)
-    c = conn.cursor()
-    conversations=[]
-    for row in c.execute("SELECT * FROM conversations WHERE user1=?",[user]):
-        this={'me':row[1], 'to':row[2], 'log':row[3]}
-        conversations.append(this)
-    for row in c.execute("SELECT * FROM conversations WHERE user2=?",[user]):
-        this={'me':row[2],'to':row[1], 'log':row[3]}
-        conversations.append(this)
-    conn.commit()
-    conn.close()
-    return conversations
-    
+    with app.app_context():
+        conn=sqlite3.connect(db)
+        c = conn.cursor()
+        conversations=[]
+        for row in c.execute("SELECT * FROM conversations WHERE user1=?",[user]):
+            this={'me':row[1], 'to':row[2], 'log':row[3]}
+            conversations.append(this)
+        for row in c.execute("SELECT * FROM conversations WHERE user2=?",[user]):
+            this={'me':row[2],'to':row[1], 'log':row[3]}
+            conversations.append(this)
+        conn.commit()
+        conn.close()
+        return conversations
+
 #Functions----------------------------------------------------------------------
 
 #Global-------------------------------------------------------------------------
--- messanger/views.py
+++ messanger/views.py
@@ -6,23 +6,31 @@
 #------------------------------------------------------------------------------#
 
 #Imports------------------------------------------------------------------------
-from flask import render_template, request, redirect, url_for, session, flash
+import database
+from flask import render_template, request, redirect, url_for, session, flash, g
 from messanger import app
 
 #Global Deleratons--------------------------------------------------------------
-dummyConv=[{'to':1, 'log':"Lorem Ipsum"},{'to':2, 'log':"Lorem Ipsum"}]
+
 #Functions----------------------------------------------------------------------
+def getConversations():
+    with app.app_context():
+        try:
+            return database.getMyConversations(session['username'])
+        except KeyError:
+            return []
+
 def do_the_login(username,password):
     return username
 
 #Main Views
 @app.route('/')
 def index():
-    return render_template('jiberish.html', conversations=dummyConv)
+    return render_template('home.html', conversations=getConversations())
 
 @app.route('/conv')
 def conversations():
-    return render_template('conversation.html', conversations=dummyConv)
+    return render_template('conversation.html', conversations=getConversations())
 #account views
 @app.route('/accounts/login', methods=['GET','POST'])
 def login():
@@ -31,7 +39,7 @@ def login():
         session['username']=do_the_login(request.form['username'],request.form['password'])
         return redirect(url_for('index'))
     else:   
-        return render_template('login.html', conversations=dummyConv)
+        return render_template('login.html', conversations=getConversations())
 @app.route('/accounts/logout')
 def logout():
     flash('Logged Out')
@@ -40,4 +48,19 @@ def logout():
 
 
 #backend views
+@app.route('/backend/make', methods=['POST'])
+def makeConversation():
+    if session['username']:
+        database.make_conversation(session['username'],request.form[sendTo])
+        flash('Conversation to '+request.form[sendTo]+' started!')
+        return redirect(url_for('index'))
+    else:
+        flash('Please log in')
+        return redirect(url_for('index'))
+
+
+
+
 #Global-------------------------------------------------------------------------
+
+
